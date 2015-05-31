5997e3f7d91c54ac1f9e0181d0372ae673655bd1
chaubeau <chaubeau01@gmail.com>
2015-05-31 22:22:27 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# elective system
+
+# 学生选课系统
+
+---
+
+## 使用方法
+
+### Nginx服务
+
+安装部署nginx服务
+### php-fpm
+部署php-fpm服务
+
+### MySQL
+* 安装MySQL
+* 修改`models/mysql.php` 14行的MySQL链接参数
+* 导入表创建语句
+
+>* `mysql>source  config/create.sql`
+
+* 插入管理员登陆数据(username:admin,password:admin)
+
+>* `mysql> insert into admin(adminname,adminpwd) values('admin','21232f297a57a5a743894a0e4a801fc3');`
+
+### 其他使用方法
+下载docker 镜像并启动，浏览器直接访问
+
+```
+    docker run -d -P index.alauda.cn/chaubeau/91606cc1fef1:1.5
+    
+```
+
 
--- views/test.php
+++ views/test.php
@@ -1,28 +1,3 @@
-<!DOCTYPE HTML> 
-<html> 
-<head> 
-<title>HTML5+CSS3实现图片旋转</title> 
-<script type="text/javascript">  
-function startup() {  
-var canvas = document.getElementById('canvas');  
-var ctx = canvas.getContext('2d');  
-var img = new Image(); 
-img.src = 'http://statics.juxia.com/uploadfile/content/2014/10/2014101512115155.gif'; 
-img.onload = function() { 
-while ( 1 < 1){
-ctx.translate(img.width / 2, img.height / 2);  
-ctx.rotate(30 * Math.PI / 120);  
-// 120为设置旋转角度 
-ctx.drawImage(img, 0, 0, 165, 60); // 165和60分别是图片宽度高度 
-
-}  
-}  
-</script> 
-</head> 
-<body onload='startup();'> 
-<div id="pw_body" style="width:100%;height:100%"> 
-<canvas id="canvas" style="position: absolute; left: 300px; top: 100px;" width="800" height="600"></canvas> 
-</div> 
-<p>代码整理于网络</p> 
-</body> 
-</html> 
+<?php
+    echo md5("admin");
+?>
